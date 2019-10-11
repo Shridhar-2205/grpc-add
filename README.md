d54a28bfffb339d778e2860e70eab0236ff33d55
Sangeet Kumar <sk@urantiatech.com>
2019-10-11 10:20:50 +0530

    Initial checkin

---
+++ README.md
@@ -0,0 +1,18 @@
+## Generate proto/service.pb.go
+```sh
+protoc --proto_path=proto --proto_path=third_party --go_out=plugins=grpc:proto service.proto
+```
+
+## Run Server
+```sh
+cd server
+go run server.go
+```
+
+## Run Client
+```sh
+cd client
+go run client.go
+```
+
+Open the browser with url http://localhost:8080/100/200
---
+++ server/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"github.com/sangeetk/grpc-add/proto"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if err := srv.Serve(listener); err != nil {
+		panic(err)
+	}
+
+}
+
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+	result := a + b
+	return &proto.Response{Result: result}, nil
+}
